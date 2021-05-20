32fb208805f1fdaa40dafc509d0a3597f1ae99a6
Bunks <busk_1@yahoo.com>
2021-05-20 17:56:01 -0400

    java practice

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,17 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int a = 2;
+        double b = 3.5;
+        double sum = a + b;
+        String c = "cat";
+        String concat = a + c;
+
+        // Checking sum
+        System.out.println(sum);
+        System.out.println(concat);
+
+    }
+
+}
