2383581966864ac72669c1ede6e5ee844709ee09
Bunks <busk_1@yahoo.com>
2021-05-21 17:16:53 -0400

    added query param

--- src/main/java/com/technews/repository/PostRepository.java
+++ src/main/java/com/technews/repository/PostRepository.java
@@ -8,5 +8,7 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
+
     List<Post> findAllPostsByUserId(Integer id) throws Exception;
+
 }
